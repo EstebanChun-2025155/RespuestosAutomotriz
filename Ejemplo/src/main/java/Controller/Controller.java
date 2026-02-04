5f391120fe110021ad554997d68dca903e522f21
Estebanchun-2025155 <echun-2025155@kinal.edu.gt>
2026-02-03 19:23:17 -0600

    feat: Se cargo carpeta del proyecto

---
+++ Ejemplo/src/main/java/Controller/Controller.java
@@ -0,0 +1,10 @@
+package Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/controller")
+    public String controller() {return "Prueba no.1 ";}
+}
